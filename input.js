80bb2f467a5315279c1988f54809ef59217449e6
alisatelier <194897761+alisatelier@users.noreply.github.com>
2025-07-09 00:44:55 -0600

    separated setupInput and handleUserInput into input.js file to refactor.

---
+++ input.js
@@ -0,0 +1,22 @@
+// setup for interface to handle user input from stdin
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+// message for exiting the game via: CTRL + C 
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    console.log("see ya!");
+    process.exit();
+  }
+};
+
+
+module.exports = {
+  setupInput, 
+}
