4bba1cd6fa5516e5259d19d3441409d0c02f0073
alisatelier <194897761+alisatelier@users.noreply.github.com>
2025-07-09 00:30:07 -0600

    Implemented setupInput to output a reply when game is exited CTRL + C

--- play.js
+++ play.js
@@ -2,4 +2,21 @@ const connect = require("./client.js");
 
 console.log("Connecting...");
 
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    console.log("see ya!");
+    process.exit();
+  }
+};
+
 connect();
+setupInput();
