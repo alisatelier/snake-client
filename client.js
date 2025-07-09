f78c9bb99a2f884932785664b388590f5f602a2a
alisatelier <[194897761+alisatelier@users.noreply.github.com]>
2025-07-08 23:52:51 -0600

    sent Name: L8R to server, identifying name of client.

--- client.js
+++ client.js
@@ -12,6 +12,14 @@ const connect = function() {
     console.log(data);
   });
 
+  conn.on("connect", () => {
+    console.log("sup loser. ps the snek knows you here now.");
+  });
+
+  conn.on("connect", () => {
+    conn.write("Name: L8R");
+  });
+
   return conn;
 };
 
