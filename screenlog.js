6b62dbc481b57248be8bb312979938762871177b
alisatelier <194897761+alisatelier@users.noreply.github.com>
2025-07-10 09:03:48 -0600

    updated host server to allow broadcasting by multiple players: unable to test mutliplayer at this time

---
+++ screenlog.js
@@ -0,0 +1,17 @@
+// prevents broadcasts from staying on the screen and affecting game visibility.
+
+const logTemporary = (message, duration = 5000) => {
+  const lines = message.split('\n');
+  for (const line of lines) {
+    process.stdout.write(`${line}\n`);
+  }
+
+  setTimeout(() => {
+    for (let i = 0; i < lines.length; i++) {
+      process.stdout.write('\x1b[1A'); // move up
+      process.stdout.write('\x1b[2K'); // clear
+    }
+  }, duration);
+};
+
+module.exports = logTemporary;
